9fbfaaa208a0acaa5241012adbd4dc701c7ce3a0
thiagofarias-dev <thiagofarias13@hotmail.com>
2022-11-28 23:21:32 -0300

    Chapter: Functions - 4 - Fortune Cookie

---
+++ src/main/kotlin/CH 04-07 - Fortune Cookie.kt
@@ -0,0 +1,22 @@
+fun main() {
+    for (i in 1..10) {
+        val fortune = getFortuneCookie()
+        println("Your fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
+}
+
+fun getFortuneCookie(): String {
+    val fortuneList = listOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
+    )
+    print("Enter your birthday: ")
+    val birthday = readLine()?.toIntOrNull() ?: 1
+    return fortuneList[birthday.rem(fortuneList.size)]
+}
