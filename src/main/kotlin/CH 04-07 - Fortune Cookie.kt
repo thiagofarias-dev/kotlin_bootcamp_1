41f5cca1b700a52f0554239b1ba6eda6df3085f3
thiagofarias-dev <thiagofarias13@hotmail.com>
2022-11-29 22:18:38 -0300

    Chapter: Functions - 4--9 Alterado o modo de como recebe a data de aniversário e colocado novas condições com WHEN e IF

--- src/main/kotlin/CH 04-07 - Fortune Cookie.kt
+++ src/main/kotlin/CH 04-07 - Fortune Cookie.kt
@@ -1,12 +1,12 @@
 fun main() {
     for (i in 1..10) {                          // laço para repetição de 10 vezes
-        val fortune = getFortuneCookie()            // chama a função que retorna o conteúdo do cookie
+        val fortune = getFortuneCookie(getBirthday())            // chama a função que retorna o conteúdo do cookie
         println("Your fortune is: $fortune")
         if (fortune.contains("Take it easy")) break  // sai do laço caso a variável contenha "Take easy"
     }
 }
 
-fun getFortuneCookie(): String {
+fun getFortuneCookie(birthday: Int): String {
     val fortuneList = listOf(
         "You will have a great day!",
         "Things will go well for you today.",
@@ -16,7 +16,17 @@ fun getFortuneCookie(): String {
         "Take it easy and enjoy life!",
         "Treasure your friends because they are your greatest fortune."
     )
+
+    val fortuneIdx = when (birthday) {
+        in 1..7 -> 1
+        28, 31 -> 2
+        else -> birthday.rem(fortuneList.size)
+    }
+    return fortuneList[fortuneIdx]      // retorna o valor do cookie com base no index do array, que é escolhido pelo restante da divisão
+    //return fortuneList[birthday.rem(fortuneList.size)]      // retorna o valor do cookie com base no index do array, que é escolhido pelo restante da divisão
+}
+
+fun getBirthday(): Int {
     print("Enter your birthday: ")
-    val birthday = readLine()?.toIntOrNull() ?: 1       // recebe a entrada do usuário, transforma para Int, se for nulo, seta o valor em 1
-    return fortuneList[birthday.rem(fortuneList.size)]      // retorna o valor do cookie com base no index do array, que é escolhido pelo restante da divisão
+    return readLine()?.toIntOrNull() ?: 1       // recebe a entrada do usuário, transforma para Int, se for nulo, seta o valor em 1
 }
