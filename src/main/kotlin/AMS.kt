e06c16138d95c09014aa77c629492646cfc45569
thiagofarias-dev <thiagofarias13@hotmail.com>
2022-11-29 21:33:27 -0300

    Chapter: Functions - 8 - FishFood update

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -7,11 +7,27 @@ fun main(args: Array<String>) {
 
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println("Today is $day and the fish eat $food")
 }
 
-fun randomDay() : String {
-    val week = listOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+    var food = "fasting"
+
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+
 }
